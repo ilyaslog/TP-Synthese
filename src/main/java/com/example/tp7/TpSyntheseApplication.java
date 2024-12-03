7df68fbde4612c15dc2299c7d14b96fe08886205
ilyaslog <himitilyas@gmail.com>
2024-12-03 11:00:09 +0100

    First commit

---
+++ src/main/java/com/example/tp7/TpSyntheseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSyntheseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSyntheseApplication.class, args);
+	}
+
+}
