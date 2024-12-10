d5f663f996298d7e5c29fc3182d2caf2e202fc31
ilyaslog <himitilyas@gmail.com>
2024-12-10 11:50:23 +0100

    template

--- src/main/java/com/example/tp7/Controller/DevelopperController.java
+++ src/main/java/com/example/tp7/Controller/DevelopperController.java
@@ -1,32 +1,44 @@
 package com.example.tp7.Controller;
-
 import com.example.tp7.Service.DevelopeurService;
 import com.example.tp7.entity.Developeur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/developper")
 public class DevelopperController {
+
+    private final DevelopeurService developeurService;
+
     @Autowired
     public DevelopperController(DevelopeurService developeurService) {
         this.developeurService = developeurService;
     }
 
-    private DevelopeurService developeurService;
-
-    @GetMapping("/list")
-    public String ShowAddFormDev(Model leModel) {
+    // Show form to add a new developer
+    @GetMapping("/showForm")
+    public String showAddFormDev(Model model) {
+        // Create a new Developeur object to bind to the form
         Developeur developeur = new Developeur();
-        leModel.addAttribute("developeur", developeur);
-        return "Dev-form";
+        model.addAttribute("developeur", developeur);
+        return "Dev-form"; // Return the name of the Thymeleaf template
     }
 
+    // Save developer details from the form
     @PostMapping("/save")
     public String saveDev(@ModelAttribute("developeur") Developeur developeur) {
+        // Save the developer using the service
         developeurService.save(developeur);
+        // Redirect to a listing page after saving
         return "redirect:/developper/list";
+    }
+
+    // List all developers (optional, if you want to implement a list page)
+    @GetMapping("/list")
+    public String listDeveloppers(Model model) {
+        model.addAttribute("developpers", developeurService.findAll());
+        return "Dev-list"; // Replace with your actual Thymeleaf template for the list page
     }
 }
